a2a770e11ea3daee0409fa79fd7bc3e021f96918
Brayzonn <brayzonn@gmail.com>
2026-03-06 23:18:36 +0100

    fix: remove unused ThemeProvider that referenced non-existent exports

--- client/src/contexts/ThemeProvider.tsx
+++
@@ -1,8 +0,0 @@
-import { ThemeContext, useThemeProvider } from "../hooks/useTheme";
-
-export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
-  const theme = useThemeProvider();
-  return (
-    <ThemeContext.Provider value={theme}>{children}</ThemeContext.Provider>
-  );
-};
