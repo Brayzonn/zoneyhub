78232fa159af350b2ed9c6129cceeb2e0dbe3ad5
Brayzonn <brayzonn@gmail.com>
2026-07-10 13:49:36 +0100

    chore: drop DM Sans and Cabinet Grotesk, commit to journal font roles

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,6 @@
 import React from "react";
 import App from "./App.tsx";
 
-import "@fontsource/dm-sans/400.css";
-import "@fontsource/dm-sans/500.css";
-import "@fontsource/dm-sans/600.css";
-import "@fontsource/dm-sans/800.css";
 import "@fontsource/lora/400.css";
 import "@fontsource/lora/500.css";
 import "@fontsource/reenie-beanie/400.css";
